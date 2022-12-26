e667eaf03f0e6cedd14a18461426811aac1b6075
prasanthkannanlive <103733121+prasanthkannanlive@users.noreply.github.com>
2022-12-26 18:41:38 +0400

    string formatting

--- 02_Type_System/string_formatting.py
+++ 02_Type_System/string_formatting.py
@@ -12,3 +12,8 @@ print(f"{name} is {age} years old.")
 print(f"{name} has a dog named {dog}.")
 print(f"{name} has a cat named {cat}.")
 
+
+print(f"hello {name}!".capitalize())
+print(f"{name} is {age} years old.".title())
+print(f"{name} has a dog named {dog}.".upper())
+print(f"{name} has a cat named {cat}.".lower())
